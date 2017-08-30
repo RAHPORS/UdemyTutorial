5cb5e2417d9cccb526444b42875d79c1cc91112d
rahs2406 <ramon.junio94@gmail.com>
2017-08-30 15:44:06 -0500

    Interceptor para logs de tiempo de respuesta

---
+++ src/main/java/com/rahpors/UdemyTutorial/configuration/WebMvcConfiuration.java
@@ -0,0 +1,23 @@
+package com.rahpors.UdemyTutorial.configuration;
+
+import com.rahpors.UdemyTutorial.componet.RequestTimeInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by ramon on 30/08/17.
+ */
+
+@Configuration
+public class WebMvcConfiuration extends WebMvcConfigurerAdapter{
+    @Autowired
+    @Qualifier("requestTimeInterceptor")
+    private RequestTimeInterceptor requestTimeInterceptor;
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(requestTimeInterceptor);
+    }
+}
