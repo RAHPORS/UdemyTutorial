2e3dc223912ddc8052ef28924ec4804d1654385d
rahs2406 <ramon.junio94@gmail.com>
2017-08-30 10:08:04 -0500

    Get forma 2

--- src/main/java/com/rahpors/UdemyTutorial/controller/Example2Controller.java
+++ src/main/java/com/rahpors/UdemyTutorial/controller/Example2Controller.java
@@ -3,10 +3,7 @@ package com.rahpors.UdemyTutorial.controller;
 import com.rahpors.UdemyTutorial.model.Person;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
@@ -21,11 +18,21 @@ public class Example2Controller {
 
     public static final String EXAMPLE_VIEW = "example2";
 
-    @RequestMapping(value = "/example1", method = RequestMethod.GET)
+    //Forma 1
+    //hostame/appname/example2/example1?nm=RAMON
+    @RequestMapping(value = "/request1", method = RequestMethod.GET)
     public ModelAndView request1(@RequestParam(name = "nm", required = false, defaultValue = "NULL") String name){
         ModelAndView modelAndView = new ModelAndView(EXAMPLE_VIEW);
         modelAndView.addObject("nm_inmodel",name);
         return modelAndView;
     }
 
+    //forma 2
+    //hostame/appname/example2/example1/RAMON
+    @RequestMapping(value = "/request2/{nm}", method = RequestMethod.GET)
+    public ModelAndView request2(@PathVariable("nm") String name){
+        ModelAndView modelAndView = new ModelAndView(EXAMPLE_VIEW);
+        modelAndView.addObject("nm_inmodel",name);
+        return modelAndView;
+    }
 }
