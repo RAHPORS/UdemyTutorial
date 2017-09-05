ed647bdf5fd15b04c985d94398999809ee5ee8db
rahs2406 <ramon.junio94@gmail.com>
2017-09-05 17:26:15 -0500

    Spring Security (Entity: User & UserRole)

---
+++ src/main/java/com/rahpors/UdemyTutorial/entity/UserRole.java
@@ -0,0 +1,56 @@
+package com.rahpors.UdemyTutorial.entity;
+
+import javax.persistence.*;
+
+/**
+ * Created by ramon on 5/09/17.
+ */
+
+@Entity
+@Table(name = "user_role", uniqueConstraints = @UniqueConstraint(columnNames = {"role","username"}))
+public class UserRole {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "user_role_id", unique = true, nullable = false)
+    private Integer userRoleId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "username", nullable = false)
+    private User user;
+
+    @Column(name = "role", nullable = false, length = 45)
+    private String role;
+
+    public UserRole() {
+    }
+
+    public UserRole(User user, String role) {
+        this.user = user;
+        this.role = role;
+    }
+
+    public Integer getUserRoleId() {
+        return userRoleId;
+    }
+
+    public void setUserRoleId(Integer userRoleId) {
+        this.userRoleId = userRoleId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
